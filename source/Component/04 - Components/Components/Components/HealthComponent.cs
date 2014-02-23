55c9bfa90fd765ea5a4d23bdd1d73273271b55e9
Takumii <piresjulien95@gmail.com>
2014-02-23 17:34:21 +0100

    Component demo 04 - Components added

---
+++ source/Component/04 - Components/Components/Components/HealthComponent.cs
@@ -0,0 +1,13 @@
+﻿using Pulsar.Components;
+
+namespace Components.Components
+{
+    public sealed class HealthComponent : Component
+    {
+        #region Properties
+
+        public int Life { get; set; }
+
+        #endregion
+    }
+}
---
+++ source/Component/04 - Components/Components/Components/ShieldComponent.cs
@@ -0,0 +1,13 @@
+﻿using Pulsar.Components;
+
+namespace Components.Components
+{
+    public sealed class ShieldComponent : Component
+    {
+        #region Properties
+
+        public int Power { get; set; }
+
+        #endregion
+    }
+}
